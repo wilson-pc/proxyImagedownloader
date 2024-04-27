64003c0cae17f39713b2c96ef845b4da543d2440
Wilver Hidalgo <wilver.hidalgo@believesol.com>
2024-04-26 22:23:53 -0400

    update fastify

--- api/serverless.js
+++ api/serverless.js
@@ -1,19 +1,35 @@
 "use strict";
-
-// Read the .env file.
-import * as dotenv from "dotenv";
-dotenv.config();
-
-// Require the framework
-import Fastify from "fastify";
+import Fastify from 'fastify'
+import axios from "axios"
 
 // Instantiate Fastify with some config
-const app = Fastify({
-    logger: true,
-});
+const app = Fastify();
+app.get('/', async function handler(request, reply) {
+    return { message: "hello" }
+})
+app.get('/proxy', async function handler(request, reply) {
+    if (request.query.remote) {
+        try {
+            const url = new URL(request.query.remote.trim())
 
-// Register your application as a normal plugin.
-app.register(import("../src"));
+            const { data } = await axios.get(url, {
+                responseType: 'arraybuffer',
+            });
+
+            if (data.byteLength > 90682) {
+                reply.type('image/png')
+                reply.send(data)
+            } else {
+                reply.code(404).send({ error: "not link" })
+            }
+        } catch (error) {
+            reply.code(404).send({ error: "not link" })
+        }
+    } else {
+        reply.code(404).send({ error: "not link" })
+    }
+})
+
 
 export default async (req, res) => {
     await app.ready();
--- src/index.js
+++ src/index.js
@@ -5,15 +5,32 @@ import axios from "axios"
 const fastify = Fastify()
 // Declare a route
 fastify.get('/', async function handler(request, reply) {
-    const url = new URL(request.query.remote)
-
-    const { data } = await axios.get(url, {
-        responseType: 'arraybuffer',
-    });
-
-    reply.type('image/png')
-    reply.send(data)
+    return { message: "hello" }
 })
+
+fastify.get('/proxy', async function handler(request, reply) {
+    if (request.query.remote) {
+        try {
+            const url = new URL(request.query.remote.trim())
+
+            const { data } = await axios.get(url, {
+                responseType: 'arraybuffer',
+            });
+
+            if (data.byteLength > 90682) {
+                reply.type('image/png')
+                reply.send(data)
+            } else {
+                reply.code(404).send({ error: "not link" })
+            }
+        } catch (error) {
+            reply.code(404).send({ error: "not link" })
+        }
+    } else {
+        reply.code(404).send({ error: "not link" })
+    }
+})
+
 
 // Run the server!
 try {
